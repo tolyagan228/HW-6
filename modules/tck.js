2cb2bffde8c6c2f05fa2b6ec27244dcff2dd916e
tolyagan228 <144461388+tolyagan228@users.noreply.github.com>
2025-04-16 19:27:33 +0300

    finish

--- modules/tck.js
+++ modules/tck.js
@@ -1,19 +1,19 @@
 export function tck() {
-  alert("Это ТЦК, предьявите Ваши документы");
+  alert("Это ТЦК, предъявите Ваши документы");
 
   const text = document.createElement("h1");
   text.classList.add("title");
   text.textContent = "Центр ТЦК";
   document.body.appendChild(text);
 
-  const img = document.createElement("img");
-  img.classList.add("img");
-  img.src = "./images/tck.jpg";
-  container.appendChild(img);
-
   const container = document.createElement("div");
   container.classList.add("container");
   document.body.appendChild(container);
+
+  const img = document.createElement("img");
+  img.classList.add("img");
+  img.src = "./images/tckhello.jpg";
+  container.appendChild(img);
 
   const btn = document.createElement("button");
   btn.classList.add("btn");
